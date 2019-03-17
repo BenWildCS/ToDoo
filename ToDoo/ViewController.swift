58719b8f37a96a69cf17c5620a145205133bfb84
Benjamin Csaszta <ben@benjamin-macbook-pro.local>
2019-03-17 08:53:42 +0100

    Initial Commit

---
+++ ToDoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoo
+//
+//  Created by Benjamin Csaszta on 17/03/2019.
+//  Copyright © 2019 BenCsaszta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
